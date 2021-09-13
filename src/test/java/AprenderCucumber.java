e0477e1fc0f23cc50ae8a25620ed2566b21b10f9
thinogueiras <thiagonogueira@hotmail.com.br>
2021-09-12 23:30:08 -0300

    Projeto criado

---
+++ src/test/java/AprenderCucumber.java
@@ -0,0 +1,22 @@
+import io.cucumber.java.PendingException;
+import io.cucumber.java.en.Given;
+import io.cucumber.java.en.Then;
+import io.cucumber.java.en.When;
+
+public class AprenderCucumber {
+
+	@Given("que criei o arquivo corretamente")
+	public void que_criei_o_arquivo_corretamente() throws Throwable {
+	    
+	}
+
+	@When("executá-lo")
+	public void executá_lo() throws Throwable {
+	    
+	}
+
+	@Then("a especificação deve finalizar com sucesso")
+	public void a_especificação_deve_finalizar_com_sucesso() throws Throwable {
+	    
+	}
+}
---
+++ src/test/java/aprender_cucumber.feature
@@ -0,0 +1,6 @@
+Feature: Aprender Cucumber
+
+Scenario: Deve executar especificação
+	Given que criei o arquivo corretamente
+	When executá-lo
+	Then a especificação deve finalizar com sucesso
